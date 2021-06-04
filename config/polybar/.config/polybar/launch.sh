e3d69498054ed83ff203a08a7e363e714fd6ba06
mmskv <max.mishuk2014@gmail.com>
2021-06-04 20:34:31 +0300

    Remove background color

---
+++ config/polybar/.config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch primary and secondary bars
+polybar -c ~/.config/polybar/config-primary.ini primary &
+# polybar -c ~/.config/polybar/config-primary.ini systray &
