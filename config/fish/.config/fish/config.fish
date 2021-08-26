189a20e90383e0540e22b356568f03a901a65ac2
mmskv <max.mishuk2014@gmail.com>
2021-08-26 21:43:53 +0300

    Remove redundant lines, add comments

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -1,9 +1,9 @@
-#cat ~/.cache/wal/sequences > /dev/null &
-fish_vi_key_bindings &
-
+## Fix for curses applications where terminal would not update 
+## $COLUMNS and $LINES
 set --unexport COLUMNS
 set --unexport LINES
 
+## Bold colors
 set fish_color_command ccccc --bold
 set fish_color_error e84f4f
 set fish_color_quote b7ce42 
@@ -24,8 +24,8 @@ alias mv "mv -iv"
 alias rm "rm -v"
 
 alias librespot "/home/overaid/github/librespot/target/release/librespot -n Ono-Sendai"
-alias 'commit'='set -x GPG_TTY (tty); git commit -S'
-alias gitlog "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias 'commit'='set -x GPG_TTY (tty); git commit -Sv'
+alias gitlog "PAGER=less git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 ## Keybinding
 set fish_key_bindings fish_vi_key_bindings
@@ -36,7 +36,7 @@ set -x EDITOR /usr/bin/nvim
 set -x PAGER /usr/bin/nvimpager
 set -x BROWSER /usr/bin/qutebrowser
 
-# Start X at login
+## Start X at login
 if status --is-login
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
     exec startx
