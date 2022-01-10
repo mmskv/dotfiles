4bc77481d8897e792feaab7f8946690a7fc9ba0a
mmskv <max.mishuk2014@gmail.com>
2022-01-11 02:16:13 +0300

    New color theme

--- scripts/dmenu.sh
+++ scripts/dmenu.sh
@@ -2,21 +2,8 @@
 
 # Import the colors
 
-color0='#393939'
-color1='#da4939'
-color2='#9acc79'
-color3='#d0d26b'
-color4='#6d9cbe'
-color5='#9f5079'
-color6='#435d75'
-color7='#c2c2c2'
-color8='#474747'
-color9='#da4939'
-color10='#9acc79'
-color11='#d0d26b'
-color12='#6d9cbe'
-color13='#9f5079'
-color14='#435d75'
-color15='#c2c2c2'
+color0='#0D0E0F'
+color1='#dc322f'
+color15='#dee3e7'
 
-dmenu_run -nb "$color0" -nf "$color15" -sb "$color3" -sf "$color0"
+dmenu_run -nb "$color0" -nf "$color15" -sb "$color1" -sf "$color15"
