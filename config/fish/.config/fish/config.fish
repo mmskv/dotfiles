f8b8ece7c67c523e378ec45d776ee090fd8a2e6f
mmskv <max.mishuk2014@gmail.com>
2022-01-11 02:09:52 +0300

    Replace bspwm with riverwm

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -18,14 +18,9 @@ alias grep "grep --color=auto -i"
 alias clone "git clone --depth 1"
 alias ra "ranger"
 
-alias euses "equery uses"
 alias cp "cp -iv"
 alias mv "mv -iv"
 alias rm "rm -v"
-
-alias librespot "/home/overaid/github/librespot/target/release/librespot -n Ono-Sendai"
-alias 'commit'='set -x GPG_TTY (tty); git commit -Sv'
-alias gitlog "PAGER=less git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 ## Keybinding
 set fish_key_bindings fish_vi_key_bindings
@@ -33,12 +28,16 @@ set fish_key_bindings fish_vi_key_bindings
 ## Locale
 set -x LC_ALL en_US.utf8
 set -x EDITOR /usr/bin/nvim
-set -x PAGER /usr/bin/nvimpager
 set -x BROWSER /usr/bin/qutebrowser
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+## XKB
+set -x XKB_DEFAULT_OPTIONS caps:escape,grp:alt_space_toggle
+set -x XKB_DEFAULT_LAYOUT us,ru
 
 ## Start X at login
 if status --is-login
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
-    exec startx
+    exec river
   end
 end
