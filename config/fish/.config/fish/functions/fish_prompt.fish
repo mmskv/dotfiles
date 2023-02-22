4ba5e473267382ea52a84cd16ef6521533edbd6e
mmskv <max.mishuk2014@gmail.com>
2023-02-23 02:14:45 +0300

    Copy fish config from desktop

--- config/fish/.config/fish/functions/fish_prompt.fish
+++ config/fish/.config/fish/functions/fish_prompt.fish
@@ -1,11 +1,17 @@
-function fish_prompt --description 'Screen Savvy prompt'
+function fish_prompt
     if test -z "$WINDOW"
-        if git rev-parse --is-inside-work-tree > /dev/null 2>&1
-            printf '%s%s %s%s%s%s%s%s%s ' (set_color -o F0C674) $USER (set_color $fish_color_cwd) (prompt_pwd) (set_color B5BD68) (fish_git_prompt) (set_color -o 8ABEB7) (echo ">") (set_color normal)
-        else
-            printf '%s%s %s%s%s%s%s ' (set_color -o F0C674) $USER (set_color $fish_color_cwd) (prompt_pwd) (set_color -o 8ABEB7) (echo -n ">") (set_color normal)
+        if test -e /.dockerenv
+            printf '%s%s%s%s%s ' \( (set_color -o brred) docker (set_color normal) \)
         end
+
+        printf '%s%s %s%s%s' (set_color -o brmagenta) $USER (set_color blue) (prompt_pwd) 
+
+        if git rev-parse --is-inside-work-tree > /dev/null 2>&1
+            printf '%s%s%s' (set_color normal) (set_color green) (fish_git_prompt)
+        end
+ 
+        printf '%s%s%s ' (set_color -o brwhite) (echo -n ">") (set_color normal)
     else
-        printf '%s%s@%s%s%s(%s)%s%s%s> ' (set_color yellow) $USER (set_color purple) (prompt_hostname) (set_color white) (echo $WINDOW) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+        printf '%s%s@%s%s%s(%s)%s%s%s> ' (set_color $fish_color_user) $USER (set_color $fish_color_host) (prompt_hostname) (set_color white) (echo $WINDOW) (set_color cyan) (prompt_pwd) (set_color normal)
     end
 end
